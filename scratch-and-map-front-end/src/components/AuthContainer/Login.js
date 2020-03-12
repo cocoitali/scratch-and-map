46e4a2f3bb79f524cc3bcb8aa9bf419fcefbdd29
Courtney Buratto <cmburatto@gmail.com>
2020-03-11 19:39:29 -0600

    Converting Login.js to use hooks

--- scratch-and-map-front-end/src/components/AuthContainer/Login.js
+++ scratch-and-map-front-end/src/components/AuthContainer/Login.js
@@ -1,63 +1,70 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import FbLogin from "./FbLogin";
 
-class Login extends Component {
-  constructor() {
-    super();
-    this.state = {};
-    // submitLogin(e) {
-    //
-    // }
-  }
+const Login = props => {
+  const [credentials, setCredentials] = useState({});
 
-  handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+  // const login = e => {
+  //   e.preventDefault();
+  //   axiosWithAuth()
+  //     .post("/login", credentials)
+  //     .then(res => {
+  //       localStorage.setItem("token", res.data.token);
+  //       this.props.history.push("/");
+  //     });
+  // };
+
+  const handleInputChange = e => {
+    setCredentials: ({
+        ...credentials,
+        [e.target.name]: e.target.value,
+      })
   };
 
-  render() {
-    return (
-      <div className="login-register-wrapper">
-        <div className="form-wrapper">
-          <form onSubmit={this.handleSubmit}>
-            <div className="input-wrapper">
-              <label className="login-label">Username or email</label>
-              <input
-                className="login-input"
-                name="username" //how to add a second name for email?
-                value={this.props.username}
-                placeholder=""
-                onChange={this.props.inputChange}
-                type="text"
-              />
-            </div>
+  return (
+    <div className="login-register-wrapper">
+      <div className="form-wrapper">
+        <form onSubmit={this.handleSubmit}>
+          <div className="input-wrapper">
+            <label className="login-label">Username or email</label>
+            <input
+              className="login-input"
+              name="username" //how to add a second name for email?
+              value={this.props.username}
+              placeholder=""
+              onChange={this.props.inputChange}
+              type="text"
+            />
+          </div>
 
-            <div className="input-wrapper">
-              <label className="login-label">Password</label>
-              <input
-                className="login-input"
-                name="password"
-                value={this.props.password}
-                placeholder=""
-                onChange={this.props.inputChange}
-                type="password"
-              />
-            </div>
-            <div className="input-wrapper">
-              <button
-                className="login-btn"
-                type="submit"
-                onClick={this.props.submit}
-              >
-                Log in
-              </button>
-            </div>
-            <a href="/forgotpassword" className="forgot-pwd">
-              I forgot my password
-            </a>
-          </form>
-        </div>
+          <div className="input-wrapper">
+            <label className="login-label">Password</label>
+            <input
+              className="login-input"
+              name="password"
+              value={credentials.password}
+              placeholder=""
+              onChange={this.handleInputChange}
+              type="password"
+            />
+          </div>
+          <div className="input-wrapper">
+            <button
+              className="login-btn"
+              type="submit"
+              onClick={this.props.submit}
+            >
+              Log in
+            </button>
+          </div>
+          <a href="/forgotpassword" className="forgot-pwd">
+            I forgot my password
+          </a>
+          <FbLogin />
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Login;
