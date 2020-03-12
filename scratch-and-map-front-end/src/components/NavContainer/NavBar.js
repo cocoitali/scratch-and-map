13fb5ba72b1b946d43d86814b9ea5d583f907b88
Courtney Buratto <cmburatto@gmail.com>
2020-03-11 19:55:04 -0600

    Begin to refactor NavBar using hooks

--- scratch-and-map-front-end/src/components/NavContainer/NavBar.js
+++ scratch-and-map-front-end/src/components/NavContainer/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Link } from "react-router-dom";
 import {
   Menu,
@@ -19,157 +19,153 @@ import CheckoutForm from "../CheckoutForm";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
-class NavBar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const NavBar = props => {
+  const [state, setState] = useState(props);
 
-  render() {
-    return (
-      <div>
-        <Sidebar.Pushable>
-          <Sidebar
-            as={Menu}
-            animation="overlay"
-            icon="labeled"
-            className="sidebar"
-            inverted
-            vertical
-            visible={this.props.visible}
-            width="wide"
-          >
-            <Button.Group className="closebutton">
-              <Button onClick={this.props.onToggle} icon="close" inverted />
-            </Button.Group>
-            <Menu.Item as={Link} to="/">
-              <img src={logo} alt="" />
-            </Menu.Item>
-            {this.props.isLoggedIn ? (
-              <Link
-                to={{
-                  pathname: "/map",
-                  state: {
-                    user: window.localStorage.getItem("SAMUserID")
-                  }
-                }}
+  return (
+    <div>
+      <Sidebar.Pushable>
+        <Sidebar
+          as={Menu}
+          animation="overlay"
+          icon="labeled"
+          className="sidebar"
+          inverted
+          vertical
+          visible={state.visible}
+          width="wide"
+        >
+          <Button.Group className="closebutton">
+            <Button onClick={state.onToggle} icon="close" inverted />
+          </Button.Group>
+          <Menu.Item as={Link} to="/">
+            <img src={logo} alt="" />
+          </Menu.Item>
+          {state.isLoggedIn ? (
+            <Link
+              to={{
+                pathname: "/map",
+                state: {
+                  user: window.localStorage.getItem("SAMUserID")
+                }
+              }}
+            >
+              <Menu.Item
+                onClick={() =>
+                  state.getUserData(
+                    window.localStorage.getItem("SAMUserID")
+                  )
+                }
               >
-                <Menu.Item
-                  onClick={() =>
-                    this.props.getUserData(
-                      window.localStorage.getItem("SAMUserID")
-                    )
-                  }
-                >
+                <Icon name="map" inverted />
+                My Map
+              </Menu.Item>{" "}
+            </Link>
+          ) : (
+            <Modal
+              trigger={
+                <Menu.Item>
+                  {/* onClick={onClick} */}
                   <Icon name="map" inverted />
                   My Map
-                </Menu.Item>{" "}
-              </Link>
-            ) : (
-              <Modal
-                trigger={
-                  <Menu.Item>
-                    {/* onClick={onClick} */}
-                    <Icon name="map" inverted />
-                    My Map
-                  </Menu.Item>
-                }
-                basic
-                size="large"
-                closeIcon
-              >
-                <Modal.Content>
-                  <p style={{ textAlign: "center" }}>
-                    Please Log In to Access Map
-                  </p>
-                </Modal.Content>
-              </Modal>
-            )}
+                </Menu.Item>
+              }
+              basic
+              size="large"
+              closeIcon
+            >
+              <Modal.Content>
+                <p style={{ textAlign: "center" }}>
+                  Please Log In to Access Map
+                </p>
+              </Modal.Content>
+            </Modal>
+          )}
 
-            <FriendListView />
-          </Sidebar>
-          <div className="Menu">
-            <div className="leftNav">
-              <Button
-                className="navbutton"
-                inverted
-                onClick={this.props.onToggle}
-              >
-                MENU
-              </Button>
-            </div>
-            <div className="rightNav">
-              <Modal
-                trigger={
-                  <Button inverted className="premium">
-                    <Icon name="gem" />
-                    PREMIUM
-                  </Button>
-                }
-                basic
-                size="small"
-                closeIcon
-              >
-                <Header icon="gem" content="Premium Sign Up" />
-                <Modal.Content>
-                  <CheckoutForm />
-                </Modal.Content>
-              </Modal>
-              )
-              <div className="AuthButtons">
-                {window.localStorage.getItem("SAMUserID") ? (
-                  <Button
-                    className="logout"
-                    inverted
-                    onClick={this.props.onLogout}
-                  >
-                    LOG OUT
-                  </Button>
-                ) : (
-                  <Auth />
-                )}
-              </div>
+          <FriendListView />
+        </Sidebar>
+        <div className="Menu">
+          <div className="leftNav">
+            <Button
+              className="navbutton"
+              inverted
+              onClick={state.onToggle}
+            >
+              MENU
+            </Button>
+          </div>
+          <div className="rightNav">
+            <Modal
+              trigger={
+                <Button inverted className="premium">
+                  <Icon name="gem" />
+                  PREMIUM
+                </Button>
+              }
+              basic
+              size="small"
+              closeIcon
+            >
+              <Header icon="gem" content="Premium Sign Up" />
+              <Modal.Content>
+                <CheckoutForm />
+              </Modal.Content>
+            </Modal>
+            )
+            <div className="AuthButtons">
+              {window.localStorage.getItem("SAMUserID") ? (
+                <Button
+                  className="logout"
+                  inverted
+                  onClick={state.onLogout}
+                >
+                  LOG OUT
+                </Button>
+              ) : (
+                <Auth />
+              )}
             </div>
           </div>
+        </div>
 
-          <Sidebar.Pusher
-            dimmed={this.props.visible}
-            onClick={this.props.onPusherClick}
-            style={{ minHeight: "100vh" }}
-          >
-            <Segment basic>
-              <Route path="/" exact render={props => <Landing />} />
-              <Route path="/map" exact render={props => <MapContainer />} />
-            </Segment>
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
+        <Sidebar.Pusher
+          dimmed={state.visible}
+          onClick={state.onPusherClick}
+          style={{ minHeight: "100vh" }}
+        >
+          <Segment basic>
+            <Route path="/" exact render={props => <Landing />} />
+            <Route path="/map" exact render={props => <MapContainer />} />
+          </Segment>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
 
-        <div className="footer" id="footer">
-          <Modal
-            trigger={
-              <div className="contact">
-                <p> ABOUT THE TEAM </p>
+      <div className="footer" id="footer">
+        <Modal
+          trigger={
+            <div className="contact">
+              <p> ABOUT THE TEAM </p>
+            </div>
+          }
+          basic
+          size="small"
+          closeIcon
+        >
+          <Header
+            content={
+              <div style={{ display: "flex" }}>
+                <p className="teamtext">THE TEAM</p>
               </div>
             }
-            basic
-            size="small"
-            closeIcon
-          >
-            <Header
-              content={
-                <div style={{ display: "flex" }}>
-                  <p className="teamtext">THE TEAM</p>
-                </div>
-              }
-            />
-            <Modal.Content>
-              <DevCard />
-            </Modal.Content>
-          </Modal>
-        </div>
+          />
+          <Modal.Content>
+            <DevCard />
+          </Modal.Content>
+        </Modal>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   return {
